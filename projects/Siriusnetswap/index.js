010174ed905fb5474961ccfbba572262ebcbdc45
Siriusnet <110954486+SiriusnetInstitute@users.noreply.github.com>
2024-01-14 12:50:03 +0800

    Update index.js

--- projects/Siriusnetswap/index.js
+++ projects/Siriusnetswap/index.js
@@ -1,1 +1,9 @@
+const {uniTvlExport} = require('../helper/calculateUniTvl.js')
 
+const factory = '0xb6f92926227118998F52a7e1768b77c437B30705'
+
+module.exports = {
+  Siriusnet:{
+    tvl: uniTvlExport(factory, 'Siriusnet', true),
+  },
+}
